905a03dad1eb808b85474821461b542f2ddf893e
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-07-01 09:27:59 -0500

    add first users/registrations_controller test

---
+++ spec/requests/users/registrations_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Users::Registrations", type: :request do
+    describe 'get /users/sign_up' do
+        it 'renders a sign up form' do
+            get '/users/sign_up'
+
+            expect(response.status).to eq(200)
+            expect(response.body).to include('Welcome.')
+            expect(response.body).to include('Email')
+            expect(response.body).to include('Password')
+            expect(response.body).to include('Password confirmation')
+        end
+    end
+
+end

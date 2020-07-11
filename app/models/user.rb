89b35cb38352813e901ed711a7bdc887416ec782
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-07-11 12:23:08 -0500

    update associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :messages, dependent: :destroy
-  has_many :channels, through: :messages
 end
